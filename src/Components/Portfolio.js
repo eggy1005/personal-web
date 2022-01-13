5adc316a7a23b126de043105aba67980e8fa33b5
eggy1005 <eggy1005@hotmail.com>
2022-01-13 23:36:28 +0000

    test

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Player from 'griffith'
+
+import ReactPlayer from 'react-player'
 
 class Portfolio extends Component {
   render() {
@@ -20,8 +21,23 @@ class Portfolio extends Component {
 
         <h1 >Check out my video here</h1>
 <div >
-           <div className="row education">
-            <Player sources={sources} />
+          <div className="row education">
+    
+          <div style={{ height: '65vh' }}>
+        <ReactPlayer
+          url="https://youtu.be/kMCjXot6jxM"
+          className="react-player"
+          playing={false}
+          width="100%"
+          height="100%"
+        />
+      </div>
+
+            {/* <Player
+                 playsInline
+                 poster="/assets/poster.png"
+                 src="/videos/new year letter.MOV"/> */}
+
             </div>
       </div>
 
