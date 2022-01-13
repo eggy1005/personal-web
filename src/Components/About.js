ae9cf9d5a72c77bcfd74f09824f36a76fb16593d
eggy1005 <eggy1005@hotmail.com>
2022-01-13 21:33:42 +0000

    feature it

--- src/Components/About.js
+++ src/Components/About.js
@@ -34,14 +34,8 @@ class About extends Component {
 						   <span>{street}<br />
 						         {city} {state}, {zip}
                    </span><br />
-						   <span>{phone}</span><br />
                      <span>{email}</span>
 					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
                </div>
             </div>
          </div>
